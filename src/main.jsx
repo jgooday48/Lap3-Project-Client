f5f60693b2304b100f2e6af37ac8b4fa33c5c2f5
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-12-17 22:09:50 +0000

    add richtexteditor component

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { registerLicense } from '@syncfusion/ej2-base'
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx0QXxbf1xzZFREallZTndcUj0eQnxTdEZiW35ZcXZQTmFfWEB1WA==');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
