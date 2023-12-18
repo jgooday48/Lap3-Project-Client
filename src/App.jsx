fccec843d628fd336b1aa241458679bc29dfff20
jgooday48 <joelgooday48@gmail.com>
2023-12-18 10:46:01 +0000

    add basic home and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Home, Login } from './pages'
 import './App.css'
 import RichTextEditor from './components/RichTextEditor'
 
@@ -9,7 +10,8 @@ function App() {
 
   return (
     <>
-     <RichTextEditor/>
+    <Home />
+     {/* <RichTextEditor/> */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { registerLicense } from '@syncfusion/ej2-base'
 
@@ -8,6 +9,8 @@ registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx0QXxbf1xzZFREallZTndcUj0eQnx
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
