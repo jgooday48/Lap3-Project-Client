41a08f363011d2e3516a3e9d376f00daf3300022
jgooday48 <joelgooday48@gmail.com>
2023-12-18 08:45:51 +0000

    add imports to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import RichTextEditor from './components/RichTextEditor'
 
 function App() {
