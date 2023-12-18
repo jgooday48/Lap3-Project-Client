6882622f4648901405425a5f04e4b4eff111ce47
jgooday48 <joelgooday48@gmail.com>
2023-12-18 12:04:55 +0000

    commit

---
+++ src/pages/SignUp/signup.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
+import { screen, render, cleanup } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import * as matchers from '@testing-library/jest-dom/matchers';
+expect.extend(matchers);
+
+
+import SignUp from '.';
+
+
+describe('Sign up display', ()=> {
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <SignUp />
+            </MemoryRouter>
+        );
+    });
+    afterEach(() => {
+        cleanup()
+    })
+
+    it('only displays one h1', () => {
+        const h1s = screen.queryAllByRole('heading', {
+            level:1
+        })
+
+        expect(h1s.length).not.toBeGreaterThan(1)
+
+    })
+
+    it('only displays one form', () => {
+        const form = screen.queryAllByRole('form')
+
+        expect(form.length).not.toBeGreaterThan(1)
+
+    })
+
+
+})
