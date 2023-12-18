490afa15a0e9ddd606c0d6df713f7a86a6957ad2
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-12-18 17:26:24 +0000

    post fail

--- src/components/RichTextEditor/index.jsx
+++ src/components/RichTextEditor/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     HtmlEditor,
     Image,
@@ -48,33 +48,59 @@ const RichTextEditor = () => {
         ]
     }
 
-  
+    const [content, setContent] = useState('')
+    const [message, setMessage] = useState('')
 
 
+    const handleContentChange = (args) => {
+         setContent(args.value);
+    }
+    
+    useEffect(() => {
+console.log("content: ", content)
+})
+
+    const handleSave = () => {
+        if (content.length > 0) {
+                fetch('http://localhost:3000/notes', {
+                method: 'POST',
+                body: JSON.stringify({Title: "Hi", Content: {content}.toString(), IsImportant: false, Section: "Test"}),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                    // 'Authorization': `Bearer ${user.token}`
+                },
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                setMessage('Content added successfully.');
+                setTimeout(() => {
+                    setMessage('')
+                }, 5000)
+            })
+            .catch((err) => {
+                console.log(err.message);
+                setMessage('There was a problem in creating your note.');
+                setTimeout(() => {
+                    setMessage('')
+                }, 5000)
+            });
+        }
+    
+}
 
     return (
         <div className="editor">
-            <RichTextEditorComponent toolbarSettings={customToolbar} height={1000}>
+            <RichTextEditorComponent toolbarSettings={customToolbar} height={1000} change={ handleContentChange}>
             
                     <Inject services={[Toolbar, Link, Image, HtmlEditor, FileManager, QuickToolbar]}></Inject>
             
-
+                         
     
                 <div>
-                <p>The Rich Text Editor is WYSIWYG ("what you see is what you get") editor useful to create and edit content, and return the valid </p>
-                <p><b>Key features:</b></p>
-                <ul>
-                    <li><p>Provides &lt;IFRAME&gt; and &lt;DIV&gt; modes</p></li>
-                    <li><p>Capable of handling markdown editing.</p></li>
-                    <li><p>Contains a modular library to load the necessary functionality on demand.</p></li>
-                    <li><p>Provides a fully customizable toolbar.</p></li>
-                    <li><p>Provides HTML view to edit the source directly for developers.</p></li>
-                    <li><p>Supports third-party library integration.</p></li>
-                    <li><p>Allows preview of modified content before saving it.</p></li>
-                    <li><p>Handles images, hyperlinks, video, hyperlinks, uploads, etc.</p></li>
-                    </ul>
+                  {content}
                 </div>
             </RichTextEditorComponent>
+              <button onClick={handleSave}>Save to Backend</button>
         </div>
     
     )
