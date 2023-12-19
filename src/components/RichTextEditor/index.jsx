cca76916b612f2e14e14045a41fdeabcd7d6ca33
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-12-19 20:05:44 +0000

    update complete

--- src/components/RichTextEditor/index.jsx
+++ src/components/RichTextEditor/index.jsx
@@ -13,7 +13,8 @@ import './index.css'
 import FolderSideBar from '../FolderSideBar'
 import { useRef, useEffect } from 'react'
 
-const RichTextEditor = () => {
+const RichTextEditor = ({ content, setContent, id }) => {
+ 
 
     const customToolbar = {
         items: [
@@ -48,62 +49,100 @@ const RichTextEditor = () => {
         ]
     }
 
-    const [content, setContent] = useState('')
+
     const [message, setMessage] = useState('')
+
+
+    // const createNewNote = () => {
+    //                 fetch('http://localhost:3000/notes', {
+    //             method: 'POST',
+    //             body: JSON.stringify({Title: "Test", Content: content}),
+    //             headers: {
+    //                 'Content-type': 'application/json; charset=UTF-8',
+    //                 // 'Authorization': `Bearer ${user.token}`
+    //             },
+    //         })
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             setMessage('Content added successfully.');
+    //             setTimeout(() => {
+    //                 setMessage('')
+    //             }, 5000)
+    //         })
+    //         .catch((err) => {
+    //             console.log(err.message);
+    //              if ( err.response.status == 409) {
+    //             console.log("409 Conflict error received. Calling updateMethod()...");
+    //          updateNote();
+    //         } 
+    //       setMessage('There was a problem in creating your note.');
+    //             setTimeout(() => {
+    //                 setMessage('')
+    //             }, 5000)
+    //         });
+    // }
+
+    // async function updateNote(){
+
+    //       const options = {
+    //         method: "PATCH",
+    //           headers: {
+    //               'Content-Type': 'application/json',
+            
+    //             //   'Authorization': `Bearer ${user.token}`
+    //           },
+         
+    //         body: JSON.stringify({
+    //             Content: content
+    //         })
+    //     }
+    //    const response = await fetch(`http://localhost:3000/notes/${id}`, options)
+    //     const data = await response.json();
+    //     // setContent(data?.Content)
+
+    // }
+    
+
+    async function updateNote() {
+        try {
+            const options = {
+            method: "PATCH",
+              headers: {
+                  'Content-Type': 'application/json',
+            
+                //   'Authorization': `Bearer ${user.token}`
+              },
+         
+            body: JSON.stringify({
+                Content: content
+            })
+        }
+            const response = await fetch(`http://localhost:3000/notes/${id}`, options);
+            console.log("update happpend")
+        const data = await response.json();
+        setContent(content);
+    } catch (error) {
+        console.error("Error updating note:", error);
+    }
+}
+
 
 
     const handleContentChange = (args) => {
          setContent(args.value);
     }
-    
-    useEffect(() => {
-console.log("content: ", content.toString())
-})
 
-    const handleSave = () => {
-      
-        if (content.length > 0) {
-            console.log("inside handleSave")
-                fetch('http://localhost:3000/notes', {
-                method: 'POST',
-                body: JSON.stringify({Title: "Test", Content: content}),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                    // 'Authorization': `Bearer ${user.token}`
-                },
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                setMessage('Content added successfully.');
-                setTimeout(() => {
-                    setMessage('')
-                }, 5000)
-            })
-            .catch((err) => {
-                console.log(err.message);
-                setMessage('There was a problem in creating your note.');
-                setTimeout(() => {
-                    setMessage('')
-                }, 5000)
-            });
-        }
-    
-    }
-    
-
-    const fetchAData = ()
 
     return (
         <div className="editor">
-            <RichTextEditorComponent toolbarSettings={customToolbar} height={1000} change={ handleContentChange}>
+            <RichTextEditorComponent key={id}  toolbarSettings={customToolbar} height={1000} change={ handleContentChange} value={content}>
             
                     <Inject services={[Toolbar, Link, Image, HtmlEditor, FileManager, QuickToolbar]}></Inject>
                
-                
-                  {content}
+    
             
             </RichTextEditorComponent>
-              <button onClick={handleSave}>Save to Backend</button>
+              <button onClick={updateNote}>Save to Backend</button>
         </div>
     
     )
