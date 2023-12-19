783582715ee45666de225b4eab6d7c769d07b40a
jgooday48 <joelgooday48@gmail.com>
2023-12-19 14:47:36 +0000

    add home page

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as RichTextEditor } from './RichTextEditor'
 
 export { default as FolderSideBar } from './FolderSideBar'
-
+export { default as PrivateRoute } from './PrivateRoute'
