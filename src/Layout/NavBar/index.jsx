bc9da2104d5cf1ec3c71890360e7169b5c802e26
“EricaIna” <erica.rirura.4you@gmail.com>
2023-12-20 17:24:19 +0000

    welcome page and loginpage styling

--- src/Layout/NavBar/index.jsx
+++ src/Layout/NavBar/index.jsx
@@ -1,89 +1,105 @@
-import React from 'react'
-import { NavLink, Outlet, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import './style.css'
-import { useLogoutMutation } from '../../slices/usersApiSlice'
-import { logout } from '../../slices/authSlice'
+import React from "react";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "./style.css";
+import { useLogoutMutation } from "../../slices/usersApiSlice";
+import { logout } from "../../slices/authSlice";
 
 const NavBar = () => {
-    const activeStyle = {
-      textDecoration: "underline",
-      color: "green"
-    }
+  const activeStyle = {
+    textDecoration: "underline",
+    color: "green",
+  };
 
-    const { userInfo } =  useSelector(state => state.auth)
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [logoutApiCall] = useLogoutMutation();
+  const { userInfo } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [logoutApiCall] = useLogoutMutation();
 
-    const logoutHandler = async () => {
-      try {
-        await logoutApiCall().unwrap();
-        dispatch(logout());
-        navigate("/login");
-      } catch (err) {
-        console.log(err)
-      }
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
     }
-  
-    return (
-      <>
+  };
+
+  return (
+    <>
       <nav>
-       
         <ul className="nav-links">
           <button class="button-4" role="button">
             <li>
-                <NavLink to="/" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Home</NavLink>
+              <NavLink
+                to="/"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Home
+              </NavLink>
             </li>
           </button>
 
           {userInfo ? (
-          <>
-            <button class="button-4" role="button">
-              <li title={userInfo.name} id="username">
-              <NavLink to="/profile">
-                My profile
-              </NavLink>
-              </li>
-            </button>
+            <>
+              <button class="button-4" role="button">
+                <li title={userInfo.name} id="username">
+                  <NavLink to="/profile">My profile</NavLink>
+                </li>
+              </button>
 
-            <button class="button-4" role="button">
-              <li>
-                <NavLink to="/notes" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Notes</NavLink>
-              </li>
-            </button>
-            <button class="button-4" role="button" onClick={logoutHandler}>
-              <li>
-                  Logout
-              </li>
-            </button>
-          </>
+              <button class="button-4 btn" role="button">
+                <li>
+                  <NavLink
+                    to="/notes"
+                    style={({ isActive }) =>
+                      isActive ? activeStyle : undefined
+                    }
+                  >
+                    Notes
+                  </NavLink>
+                </li>
+              </button>
+              <button class="button-4" role="button" onClick={logoutHandler}>
+                <li>Logout</li>
+              </button>
+            </>
           ) : (
-          <>
-            <button class="button-4" role="button">
-              <li>
-                  <NavLink to="/login" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Login</NavLink>
-              </li>
-            </button>
+            <>
+              <button class="button-4" role="button">
+                <li>
+                  <NavLink
+                    to="/login"
+                    style={({ isActive }) =>
+                      isActive ? activeStyle : undefined
+                    }
+                  >
+                    Login
+                  </NavLink>
+                </li>
+              </button>
 
-            <button class="button-4" role="button">
-              <li>
-                  <NavLink to="/register" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Sign Up</NavLink>
-              </li>
-            </button>
-          </>
+              <button class="button-4" role="button">
+                <li>
+                  <NavLink
+                    to="/register"
+                    style={({ isActive }) =>
+                      isActive ? activeStyle : undefined
+                    }
+                  >
+                    Sign Up
+                  </NavLink>
+                </li>
+              </button>
+            </>
           )}
-          
-
-
-
         </ul>
       </nav>
-  
-      <Outlet/>
 
-      </>
-    );
-  }
-  
-  export default NavBar
+      <Outlet />
+    </>
+  );
+};
+
+export default NavBar;
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,114 +1,152 @@
-
-import React, { useState, useEffect, useRef, useContext } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import Loader from '../../components/Loader'
-import { setCredentials } from '../../slices/authSlice'
-import { useUpdateUserMutation } from '../../slices/usersApiSlice'
-import { useLogoutMutation } from '../../slices/usersApiSlice'
-import { logout } from '../../slices/authSlice'
-import './profile.css'
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./profile.css";
+import { toast } from "react-toastify";
+import Loader from "../../components/Loader";
+import { setCredentials } from "../../slices/authSlice";
+import { useUpdateUserMutation } from "../../slices/usersApiSlice";
+import { useLogoutMutation } from "../../slices/usersApiSlice";
+import { logout } from "../../slices/authSlice";
 
 const Profile = () => {
-
-
   const activeStyle = {
-    backgroundColor: "#30AB9C"
-  }
+    backgroundColor: "#30AB9C",
+  };
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState("")
-  const inputRef = useRef()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const inputRef = useRef();
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-   
-  const { userInfo } = useSelector(state => state.auth);
+  const { userInfo } = useSelector((state) => state.auth);
 
   const [logoutApiCall] = useLogoutMutation();
   const [updateProfile, { isLoading }] = useUpdateUserMutation();
-  
+
   useEffect(() => {
     setName(userInfo.name);
-    setEmail(userInfo.email)
+    setEmail(userInfo.email);
   }, [userInfo.setName, userInfo.setEmail]);
 
-
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    if(password !== confirmPassword){
-      toast.error("Passwords do not match")
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
     } else {
       try {
-        console.log(userInfo._id)
-        const res = await updateProfile({ _id: userInfo._id, name, email, password }).unwrap();
-        
-        console.log(res)
-        dispatch(setCredentials({res}));
-        toast.success("Profile Updated!")
+        console.log(userInfo._id);
+        const res = await updateProfile({
+          _id: userInfo._id,
+          name,
+          email,
+          password,
+        }).unwrap();
+
+        console.log(res);
+        dispatch(setCredentials({ res }));
+        toast.success("Profile Updated!");
         await logoutApiCall().unwrap();
         dispatch(logout());
         navigate("/login");
       } catch (err) {
-        toast.error(err?.data?.message || err.error)
+        toast.error(err?.data?.message || err.error);
       }
     }
-  }
+  };
 
   useEffect(() => {
-      inputRef.current.focus()
-    },[])
-  
-    const handleNameChange = (e) => {
-      setName(e.target.value)
-    }
+    inputRef.current.focus();
+  }, []);
 
-    const handleEmailChange = (e) => {
-      setEmail(e.target.value)
-    }
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-      setPassword(e.target.value)
-    }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handleConfirmPasswordChange = (e) => {
-      setConfirmPassword(e.target.value)
-    }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
 
   return (
-    <div style={activeStyle}>
+    <div style={activeStyle} id="profileDiv">
       <h1>Update profile</h1>
-      <form aria-label='update' className='update'>
+      <div className="box">
+        <form aria-label="update" className="update">
+          <ul>
+            <input
+              type="text"
+              id="name"
+              autoComplete="off"
+              value={name}
+              placeholder="name"
+              onChange={handleNameChange}
+              ref={inputRef}
+              required
+            />
+          </ul>
 
-        <ul>
-          <input type="text" id="name" autoComplete="off" value={name} placeholder='name' onChange={handleNameChange} ref={inputRef} required/>
-        </ul>
+          <ul>
+            <input
+              type="email"
+              id="email"
+              autoComplete="off"
+              placeholder="email"
+              value={email}
+              onChange={handleEmailChange}
+              ref={inputRef}
+              required
+            />
+          </ul>
 
-        <ul>
-          <input type="email" id="email" autoComplete="off" placeholder='email' value={email} onChange={handleEmailChange} ref={inputRef} required/>
-        </ul>
+          <ul>
+            <input
+              type="password"
+              id="password"
+              autoComplete="off"
+              placeholder="password"
+              value={password}
+              onChange={handlePasswordChange}
+              ref={inputRef}
+              required
+            />
+          </ul>
 
-        <ul>
-          <input type="password" id="password" autoComplete="off" placeholder='password' value={password} onChange={handlePasswordChange} ref={inputRef} required/>
-        </ul>
+          <ul>
+            <input
+              type="password"
+              id="confirmPassword"
+              autoComplete="off"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              ref={inputRef}
+              required
+            />
+          </ul>
 
-        <ul>
-          <input type="password" id="confirmPassword" autoComplete="off" placeholder='Confirm Password' value={confirmPassword} onChange={handleConfirmPasswordChange} ref={inputRef} required/>
-        </ul>
+          {isLoading && <Loader />}
 
-        {isLoading && <Loader />}
-
-        <ul>
-          <button onClick={handleSubmit} id='updateb'>Update</button>
-        </ul>
-        
-      </form> 
+          <ul>
+            <button onClick={handleSubmit} id="updateb">
+              Update
+            </button>
+          </ul>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
