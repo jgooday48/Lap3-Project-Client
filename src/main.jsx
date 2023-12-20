80ca3af1f40637d3904ef89366fe6377a5d65f37
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-12-20 09:53:44 +0000

    push change

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import store from "./store.js"
 import { Provider } from "react-redux"
 import { registerLicense } from '@syncfusion/ej2-base'
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx0QXxbf1xzZFREallZTndcUj0eQnxTdEZiW35ZcXZQTmFfWEB1WA==');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x3THxbf1x0ZFFMZV1bRnZPMyBoS35RdURhW39edHFURGFVUUZx');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
