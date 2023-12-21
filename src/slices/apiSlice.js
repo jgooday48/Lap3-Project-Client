b78aeff12287864d649f5a01aa47f5254069d717
Khider <khider.tariq1@gmail.com>
2023-12-21 19:34:29 +0000

    change api fetch url for users

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:3000"})
+const baseQuery = fetchBaseQuery({ baseUrl: "https://reddy-server-33.onrender.com/"})
 
 export const apiSlice = createApi({
     baseQuery,
