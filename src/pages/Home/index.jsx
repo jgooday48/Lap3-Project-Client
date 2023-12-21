15d9f9297f65975de581bf982100ecd2a2c28bcc
“EricaIna” <erica.rirura.4you@gmail.com>
2023-12-21 17:21:32 +0000

    orange background removed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,41 +1,42 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { FolderSideBar, RichTextEditor } from '../../components'
-import { NavLink} from 'react-router-dom'
-import './Home.css'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FolderSideBar, RichTextEditor } from "../../components";
+import { NavLink } from "react-router-dom";
+import "./Home.css";
+
 const HomePage = () => {
-  const { userInfo } =  useSelector(state => state.auth)
+  const { userInfo } = useSelector((state) => state.auth);
 
   return (
     <>
-    {!userInfo ? (
-      <div className="homepage">
-           {/* <FolderSideBar />
+      {!userInfo ? (
+        <div className="homepage">
+          {/* <FolderSideBar />
           <FolderSideBar/> 
           
               <RichTextEditor /> */}
-            <h1>Note-Worthy</h1>
-            <p>Welcome to the note app, please login to continue!</p>
-            <ul>
-              <button className="button-4" role="button" id='homepageBtn1'>
-                  <NavLink to="/login">Login</NavLink>
-              </button>
-              <button className="button-4" role="button" id='homepageBtn2'>
-                  <NavLink to="/register">Sign Up</NavLink>
-              </button>
-            </ul>
-      </div>
-    
-    ) : (    
-    <div className='homepage'>
-      <h1>Note-Worthy</h1>
-          <p>Thanks for joining! Checkout the nav bar to see where you need to go!</p>
-    </div>
-
-    )}
+          <h1>Note-Worthy</h1>
+          <p>Welcome to the note app, please login to continue!</p>
+          <ul>
+            <button className="button-4" role="button" id="homepageBtn1">
+              <NavLink to="/login">Login</NavLink>
+            </button>
+            <button className="button-4" role="button" id="homepageBtn2">
+              <NavLink to="/register">Sign Up</NavLink>
+            </button>
+          </ul>
+        </div>
+      ) : (
+        <div className="homepage">
+          <h1>Note-Worthy</h1>
+          <p>
+            Thanks for joining! Checkout the nav bar to see where you need to
+            go!
+          </p>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomePage
-
+export default HomePage;
