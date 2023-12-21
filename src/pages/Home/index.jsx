572fcefef0a0cb5a45701a28c806c3b96839351d
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-12-21 18:36:50 +0000

    logo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,20 +3,24 @@ import { useDispatch, useSelector } from "react-redux";
 import { FolderSideBar, RichTextEditor } from "../../components";
 import { NavLink } from "react-router-dom";
 import "./Home.css";
-// import logo1 from "../../../src/assets/logo1"
+import logo2 from '../../../src/assets/logo2.png'
 
 const HomePage = () => {
   const { userInfo } = useSelector((state) => state.auth);
+
+
 
   return (
     <>
       {!userInfo ? (
         <div className="homepage">
+        
           {/* <FolderSideBar />
           <FolderSideBar/> 
           
               <RichTextEditor /> */}
             <h1 className='noteworthy'>Note-Worthy</h1>
+              
             <p>Welcome to the note app, please login to continue!</p>
             <ul>
               <button className="button-4" role="button" id='homepageBtn1'>
